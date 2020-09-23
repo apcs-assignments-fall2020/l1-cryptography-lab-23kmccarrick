d8701a49b38b9bdae3c9ab3e8672ba286252dfbc
23kmccarrick <23kmccarrick@athenian.org>
2020-09-23 02:41:48 +0000

    finished decrypt methods

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -16,13 +16,17 @@ public class Caesar {
     }
 
     public static String decryptCaesar(String message) {
-                String result = "";
+        String result = "";
         
         for (int i = 0; i < message.length(); i ++){
-            if(message.charAt(i)>96&&message.charAt(i)<123)
+            if(message.charAt(i)>99&&message.charAt(i)<123)
                 result += (char)((message.charAt(i)-97-3)%26+97);
-            else if(message.charAt(i)>65&&message.charAt(i)<91)
+            else if(message.charAt(i)>67&&message.charAt(i)<91)
                 result += (char)((message.charAt(i)-65-3)%26+65);
+            else if(message.charAt(i)>64&&message.charAt(i)<68)
+                result += (char)((message.charAt(i)-65+26-3)%26+65);
+            else if(message.charAt(i)>96&&message.charAt(i)<100)
+                result += (char)((message.charAt(i)-97+26-3)%26+97);
             else
                 result += (char)(message.charAt(i));
         }
@@ -44,12 +48,47 @@ public class Caesar {
     }
 
     public static String decryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String result = "";
+        
+        for (int i = 0; i < message.length(); i ++){
+            if(message.charAt(i)>(96+key%26)&&message.charAt(i)<123)
+                result += (char)((message.charAt(i)-97-(key%26))%26+97);
+            else if(message.charAt(i)>(64+key%26)&&message.charAt(i)<91)
+                result += (char)((message.charAt(i)-65-(key%26))%26+65);
+            else if(message.charAt(i)>64&&message.charAt(i)<(65+key%26))
+                result += (char)((message.charAt(i)-65+26-(key%26))%26+65);
+            else if(message.charAt(i)>96&&message.charAt(i)<(97+key%26))
+                result += (char)((message.charAt(i)-97+26-(key%26))%26+97);
+            else
+                result += (char)(message.charAt(i));
+        }
+        return result;
+
     }
 
 
     public static void main(String[] args) {
-        System.out.println(encryptCaesar("zebra x-rays"));
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Would you like to 'encrypt' or 'encrypt' a message?");
+        String command = scan.nextLine().trim().toLowerCase();
+
+        if (command.equals("encrypt")) {
+            System.out.println("Please enter your message to be encrypted: ");
+            String message = scan.nextLine();
+            System.out.println("Here is your encrypted message: ");
+            System.out.println(encryptCaesar(message));
+        }
+        else if (command.equals("decrypt")) {
+            System.out.println("Please enter your message to be decrypted: ");
+            String message = scan.nextLine();
+            System.out.println("Here is your decrypted message: ");
+            System.out.println(decryptCaesar(message));
+        }
+        else {
+            System.out.println("Unknown command; please type either 'encrypt' or 'decrypt'");
+        }
+
+        scan.close();
     }
 }
